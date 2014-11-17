1c8b47602b23fe7ec3965785fa3bda28b9e71e28
Grayson Wright <wright.grayson@gmail.com>
2014-11-17 11:41:18 -0800

    Generate a handle lookup URL

--- app/models/network.js
+++ app/models/network.js
@@ -1,4 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  urlForHandle: function(handle) {
+    return this.get("baseUrl") + "/" + handle;
+  }
 });
--- tests/unit/models/network-test.js
+++ tests/unit/models/network-test.js
@@ -1,15 +1,21 @@
 import {
   moduleForModel,
   test
-} from 'ember-qunit';
+} from "ember-qunit";
 
-moduleForModel('network', 'Network', {
+moduleForModel("network", "Network", {
   // Specify the other units that are required for this test.
   needs: []
 });
 
-test('it has a name', function() {
+test("it has a name", function() {
   var network = this.subject({ name: "Twitter" });
 
   equal(network.get("name"), "Twitter");
+});
+
+test("it generates the lookup URL based on the base URL and handle", function() {
+  var network = this.subject({ baseUrl: "https://twitter.com" });
+
+  equal(network.urlForHandle("bob"), "https://twitter.com/bob");
 });
